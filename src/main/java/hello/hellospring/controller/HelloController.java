4f885f6da321cdbe3032e36548295a9d35008984
Your Name <psoyeon0127@naver.com>
2023-08-08 20:54:16 +0900

    [EDIT] class response

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,39 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","spring!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        //?name=값, 이런 방식으로 파라미터 값을 넘겨야 한다.
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //html 렌더링 없이, body에 내가 쓴 문자를 바로 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //객체를 넘겨주는 경우에는 json 방식으로 데이터를 변환해서 http 응답에 반환
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    //static은 메모리를 공유하여 객체의 선언없이 호출할 수 있다.
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
