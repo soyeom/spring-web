611cd81ab436824304efb646cce8f37d273de034
Your Name <psoyeon0127@naver.com>
2023-08-11 01:17:04 +0900

    make component scan

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,8 @@
 package hello.hellospring;
 
+import hello.hellospring.controller.MemberController;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +11,11 @@ import org.springframework.context.annotation.Configuration;
 public class SpringConfig {
     @Bean
     public MemberService memberService() {
-        return new MemberService();
+        return new MemberService(memberRepository());
     }
 
-
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
